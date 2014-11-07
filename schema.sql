8562d3e50d4f10ac5c8ffb34fa0f4aa38dacb443
Cherten0K <ivan_cheretaev@inbox.ru>
2014-11-08 01:41:18 +0600

    добавил в базу таблицы о записях трат и категорий

--- schema.sql
+++ schema.sql
@@ -1,18 +1,44 @@
 pragma foreign_keys=on;
+pragma encoding = 'utf-8';
 
-drop table if exists entries;
 drop table if exists users;
+drop table if exists costs;
+drop table if exists entries;
+drop table if exists categories;
+drop table if exists sub_categories;
 
 create table users (
-    login text primary key not null,
-    password text not null
+    `login` text primary key not null,
+    `password` text not null
 );
 
 create table entries (
-  id integer primary key autoincrement,
-  user integer references users(login),
-  title text not null,
-  comment text,
-  start_date datetime,
-  end_date datetime
+  `id` integer primary key autoincrement,
+  `user` integer references users(`login`),
+  `title` text not null,
+  `comment` text,
+  `cost` integer references costs(`id_cost`),
+  `start_date` datetime,
+  `end_date` datetime
+);
+
+create table costs(
+	`id_cost` integer primary key autoincrement,
+	`time` datetime,
+	`id_category` integer references categories(`id_category`),
+	`money` integer,
+	`user` integer references users(`login`)
+);
+
+create table sub_categories(
+	`id_category` integer primary key autoincrement,
+	`name` text not null,
+	`ancessor` integer references categories(`id_category`),
+	`user` integer references users(`login`)
+);
+
+create table categories(
+	`id_category` integer primary key autoincrement,
+	`name` text not null,
+	`user` integer references users(`login`)
 );
